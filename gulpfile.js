80a3f45e2873fca80b49f0af28b18854ce43e5de
carlocordova21 <carlo_andre21@hotmail.com>
2021-07-10 22:02:37 -0500

    Proyecto finalizado entorno de desarrollo

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,16 @@ const sass = require('gulp-sass')(require('sass'));
 
 const concat = require('gulp-concat');
 
+//Utilidades CSS
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+//Utilidades JS
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
+
 const paths = {
     images: 'src/img/**/*',
     scss: 'src/scss/**/*.scss',
@@ -11,13 +21,20 @@ const paths = {
 
 function css() {
     return src(paths.scss)
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('./build/css'));
 }
 
 function javascript() {
     return src(paths.js)
+        .pipe(sourcemaps.init())
         .pipe(concat('bundle.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({suffix: '.min'}))
         .pipe(dest('./build/js'));
 }
 
